33b465c3198b19a1d84a1ef0e6c2e72f22b1cd89
LadyDeveloper <anhacorrea@hotmail.com>
2020-10-18 10:45:00 -0700

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Timer tracker</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
